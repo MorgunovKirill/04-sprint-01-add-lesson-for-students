eaf83d6cfac200351629202687ce4df75b5176c7
kirillmorgunov <kirillmorgunov@mail.ru>
2024-04-02 13:10:42 +0400

    add get request

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,10 @@
 import s from './DecksList.module.css'
+import {useEffect} from "react";
+import {decksApi} from "../decks-api";
 
 export const DecksList = () => {
+  useEffect(() => {
+    decksApi.getDecks()
+  }, [])
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -1,8 +1,40 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'https://api.flashcards.andrii.es',
+  baseURL: 'https://api.flashcards.andrii.es/',
   headers: {
     'x-auth-skip': true,
   },
 })
+
+export const decksApi = {
+  getDecks() {
+    return instance.get<GetDecksResponseType>('v2/decks')
+  },
+}
+
+export type GetDecksResponseType = {
+  items: DeckType[]
+  pagination: PaginationType
+}
+export type AuthorType = {
+  id: string
+  name: string
+}
+export type DeckType = {
+  author: AuthorType
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: string
+  updated: string
+  cardsCount: number
+}
+export type PaginationType = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
